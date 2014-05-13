2a9e044dda2bf9e01633bb3425ac7412b80f7d5d
Pzink3 <parker.zink@gmail.com>
2014-05-13 10:16:36 -0700

    Added Idle Animation To Player
    Completed milestone by adding idle animation to the player.

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -5,7 +5,9 @@ game.resources = [
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
         { name: "background-tiles", type: "image", src: "data/img/background-tiles.png" },
-	/* Atlases 
+	{ name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png" },
+        { name: "player1-spritesheet", type: "image", src: "data/img/player1-spritesheet.png" },
+        /* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
@@ -15,7 +17,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+        { name: "Level01", type: "tmx", src: "data/map/Level01.tmx" }
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -6,6 +6,11 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
 
+                me.levelDirector.loadLevel("Level01");
+                
+                var player = new game.PlayerEntity(0, 420, {});
+                me.game.add(player, this.z);
+                
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
@@ -17,6 +22,6 @@ game.PlayScreen = me.ScreenObject.extend({
 	 */
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
-		me.game.world.removeChild(this.HUD);
+		me.game.world.removeChild(this);
 	}
 });
